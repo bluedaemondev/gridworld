b4561588b08483363d8c9cc0e0d7e44bee0223dd
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-06-23 16:43:06 -0300

    camera shake + postprocessing

---
+++ Assets/_scripts - Lanosa/camera/CameraShake.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+	public Transform camTransform;
+	private float shakeDuration = 0f;
+
+	// Amplitude of the shake. A larger value shakes the camera harder.
+	public float shakeAmount = 0.7f;
+	public float decreaseFactor = 1.0f;
+
+	Vector3 originalPos;
+
+	void Awake()
+	{
+		if (camTransform == null)
+		{
+			camTransform = GetComponent<Transform>();
+		}
+	}
+
+	void OnEnable()
+	{
+		originalPos = camTransform.localPosition;
+	}
+
+	public void ShakeCameraFor(float time)
+    {
+		this.shakeDuration = time;
+    }
+
+	void Update()
+	{
+		if (shakeDuration > 0)
+		{
+			camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+			shakeDuration -= Time.deltaTime * decreaseFactor;
+		}
+		else
+		{
+			shakeDuration = 0f;
+			camTransform.localPosition = originalPos;
+		}
+	}
+}
